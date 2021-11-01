6d0fab70d9881a6a39de69ddbb4e035dfb6d99c9
Narek Maloyan <narek1110@gmail.com>
2021-11-01 23:17:35 +0300

    autogluon benchmark fit

--- autogluon/run.py
+++ autogluon/run.py
@@ -26,7 +26,8 @@ def main():
     )
     gluon_predictor.fit(
         train_data=train,
-        time_limit=10
+        time_limit=3600,
+        presets='best_quality'
     )
 
     if task_type == "binary":
